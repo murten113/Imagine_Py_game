8f029d084aa5821b9caad10bb9f7f7b963a5798f
Khranza <147272570+Khranza@users.noreply.github.com>
2025-10-27 14:11:25 +0100

    Update nfc_test.py

--- nfc_test.py
+++ nfc_test.py
@@ -7,9 +7,6 @@ reader1 = mfrc522.SimpleMFRC522()
 try:
     print("Place tag...")
     id = reader1.read()
-    x = id.split(",")
-    x1 = x[0].split("(")
-    x2 = x1[1]
-    print(x2)
+    print(id[0])
 finally:
     GPIO.cleanup()
