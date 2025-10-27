8153791b6f262a994843738acbd4d0a9314465a6
Khranza <147272570+Khranza@users.noreply.github.com>
2025-10-27 13:55:41 +0100

    Update nfc_test.py

--- nfc_test.py
+++ nfc_test.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 from mfrc522 import MFRC522
 
-reader = MFRC522()
+reader1 = MFRC522()
 
 try:
     print("Place tag...")
-    id = reader.read()
-    print(id[0])
+    status1, TagType1 = reader1.MFRC522_Request(reader1.PICC_REQIDL)
+    if status1 == reader1.MI_OK:
+        print("1 yes")
+    print("klaar")
 finally:
     GPIO.cleanup()
